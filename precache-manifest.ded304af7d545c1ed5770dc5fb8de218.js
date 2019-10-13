062f2e45bfc0146f7b68d39558fce2850c0f8471
ali1129js <ali1129js@gmail.com>
2019-10-13 17:07:34 +0200

    Updates

---
+++ precache-manifest.ded304af7d545c1ed5770dc5fb8de218.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e839e933d8ffcc782dfb08f878049b1",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "2be16550b028eb266d11",
+    "url": "/react-player/static/js/2.7058acd0.chunk.js"
+  },
+  {
+    "revision": "2f9b4fba489424194f3c",
+    "url": "/react-player/static/js/main.b3e2b21c.chunk.js"
+  },
+  {
+    "revision": "3be10be7149ba5878fe7",
+    "url": "/react-player/static/js/runtime-main.b5c18b4e.js"
+  }
+]);
